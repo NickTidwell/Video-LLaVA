c2de5065ed22ad9bddec252090a88b5dfea2fee1
Nick Tidwell <nicktidwell52@gmail.com>
2024-04-10 11:42:55 -0400

    as

--- scripts/v1_5/eval/eval_image_gqa.sh
+++ scripts/v1_5/eval/eval_image_gqa.sh
@@ -41,4 +41,4 @@ python3 scripts/convert_gqa_for_eval.py --src $output_file --dst $GQADIR/$SPLIT/
 
 cd $GQADIR
 python3 eval/eval_gqa.py --tier $SPLIT/${CKPT_NAME}/testdev_balanced \
-                         --questions /scc_cephfs/yy/lb/LLaVA-Video-YY/questions1.2/testdev_balanced_questions.json
+                         --questions testdev_all_questions.json
